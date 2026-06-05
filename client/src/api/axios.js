b4de7a37fa97e2e0ef498a6ef42d934f569444ae
aakash kumar <aakashkumar58532@gmail.com>
2026-06-05 17:11:33 +0530

    fix axios issue

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-manager-kjhs.onrender.com/api",
+  baseURL: "http://localhost:5000/api",
 });
 
 export default API;
